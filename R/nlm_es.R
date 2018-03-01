6cff9e2b5e2b0935a8ac92a27af20ba8b1a8a4a6
laurajanegraham <laurajaneegraham@gmail.com>
2018-03-01 15:45:36 +0000

    update code to work for our specific case (specify proportions, fragmentation and interspersion of S/D)

--- R/nlm_es.R
+++ R/nlm_es.R
@@ -40,7 +40,7 @@
 #'   `"Von-Neumann"` takes the four adjacent cells (i.e. left, right, upper and
 #'   lower cells).
 #'
-#' @return RasterLayer
+#' @return RasterLayer with 0, 1, 2. 0 = neutral, 1 = supply, 2 = demand
 #'
 #' @references Scherer, Cédric, et al. "Merging trait-based and individual-based
 #'   modelling: An animal functional type approach to explore the responses of
@@ -65,3 +65,126 @@
 #'
 #' @export
 
+# for now, while still being sourced in, need to load the appropriate libraries
+
+nlm_neigh <-
+  function(ncol,
+           nrow,
+           resolution = 1,
+           f_supply,
+           p_supply,
+           f_demand,
+           p_demand,
+           inter,
+           f_empty = 0.5, # I may actually just want to fix this in the code, rather than allowing it to be fixed. 
+           neighborhood = "Von-Neumann") { # think about which is the more sensible neighbourhood to take
+    
+    # Check function arguments ----
+    checkmate::assert_count(ncol, positive = TRUE)
+    checkmate::assert_count(nrow, positive = TRUE)
+    checkmate::assert_numeric(f_supply, lower = 0, upper = 1)
+    checkmate::assert_numeric(p_supply, lower = 0, upper = 1)
+    checkmate::assert_numeric(f_demand, lower = 0, upper = 1)
+    checkmate::assert_numeric(p_demand, lower = 0, upper = 1)
+    checkmate::assert_numeric(inter, lower = 0, upper = 1)
+    checkmate::assert_numeric(f_empty, lower = 0, upper = 1)
+    checkmate::assert_subset(neighborhood, c("Von-Neumann", "Moore"))
+    
+    # Determine cells per category
+    no_cells <- c(p_supply * nrow * ncol, p_demand * nrow * ncol)
+    f_vec <- c(f_supply, f_demand)
+    
+    # Create an empty matrix of correct dimensions + additional 2 rows and
+    # columns ----
+    matrix <- matrix(0, ncol + 2, nrow + 2)
+    
+    # Keep applying random clusters until all elements have a value -----
+    
+    for(i in 1:2) {
+      j <- 0
+      
+      while (j < no_cells[i]) {
+        
+        # Pick random cell within correct dimensions and with value 0 ----
+        s <- which(matrix[2:(nrow + 1), 2:(ncol + 1)] == 0, arr.ind = TRUE)
+        s <- s[sample(nrow(s), 1), ]
+        row <- as.integer(s[1]) + 1
+        col <- as.integer(s[2]) + 1
+        
+        # Check neighborhood of that cell ----
+        if (neighborhood == "Von-Neumann") {
+          adjacent <- c(
+            matrix[row - 1, col    ], # upper
+            matrix[row, col - 1], # left
+            matrix[row, col + 1], # right
+            matrix[row + 1, col    ]
+          ) # lower
+        }
+        if (neighborhood == "Moore") {
+          adjacent <- c(
+            matrix[row - 1, col - 1], # upper left
+            matrix[row - 1, col    ], # upper
+            matrix[row - 1, col + 1], # upper right
+            matrix[row, col - 1], # left
+            matrix[row, col + 1], # right
+            matrix[row + 1, col - 1], # lower left
+            matrix[row + 1, col    ], # lower
+            matrix[row + 1, col + 1]
+          ) # lower right
+        }
+        
+        # calculate r upfront to make comparison to two probs in latter step
+        r <- stats::runif(1, 0, 1)
+        # get the unique values of adjacent cells
+        u <- unique(adjacent)
+        
+        # This is where I have made the main change. We have 3 options:
+        
+        # 1. u = 0 - use f_empty
+        if (u == 0) {
+          if (r < f_empty) {
+            matrix[row, col] <- i
+            j <- j + 1
+          }
+        }
+        
+        # 2. u = [0, i] or i - use f_vec[i]
+        if (identical(u, c(0, i)) || u == i) {
+          if(r < f_vec[i]) {
+            matrix[row, col] <- i
+            j <- j + 1
+          }
+        }
+          
+        # 3. u = [0, 1, i] or [1, i] - use f_vec[i] & inter
+        if (identical(u, c(0, 1, i)) || identical(u, c(1, i))) {
+          if (r < f_vec[i] & r < inter) {
+            matrix[row, col] <- i
+            j <- j + 1
+          } 
+        }
+          
+        # Update boundary conditions
+        matrix[1, ] <- matrix[nrow + 1, ]
+        matrix[nrow + 2, ] <- matrix[2, ]
+        matrix[, 1] <- matrix[, ncol + 1]
+        matrix[, ncol + 2] <- matrix[, 2]
+      } # close while j
+    } # close for i
+    
+    # Cut additional cells and transform to raster ----
+    rndes_raster <- raster::raster(matrix[
+      1:nrow + 1,
+      1:ncol + 1
+      ])
+    
+    # specify resolution ----
+    raster::extent(rndes_raster) <- c(
+      0,
+      ncol(rndes_raster) * resolution,
+      0,
+      nrow(rndes_raster) * resolution
+    )
+
+    return(rndes_raster)
+  }
