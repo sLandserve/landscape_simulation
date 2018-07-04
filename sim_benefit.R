aeda14b304878a1d4cca102a34118226da61c613
laurajanegraham <laurajaneegraham@gmail.com>
2018-07-04 11:31:29 +0200

    updating benefit analysis with parallel compute

--- sim_benefit.R
+++ sim_benefit.R
@@ -1,19 +1,25 @@
 library(plyr)
 library(sLandserv)
 library(tidyverse)
+library(doSNOW)
 
 strt <- Sys.time()
-param_table <- read_csv("data/param_table9.csv")
-
 # 1. simulate landscapes ----
-ls_sim <- apply(param_table, 1, function(x) {
-  ls_create(nrow = x[["nrow"]], 
-            ncol = x[["ncol"]],
-            p_supply = x[["p_supply"]], 
-            p_demand = x[["p_demand"]], 
-            f_supply = x[["f_supply"]], 
-            f_demand = x[["f_demand"]], 
-            inter = x[["inter"]])})
+
+cl <- makeCluster(4) # create parellel clusters
+registerDoSNOW(cl)
+
+ls_sim <- foreach(i=1:nrow(param_table),
+                  .packages = c("sLandserv"),
+                  .export = c("param_table")) %dopar% {
+                    ls_create(nrow = param_table[i, "nrow"],
+                              ncol = param_table[i, "ncol"],
+                              p_supply = param_table[i, "p_supply"],
+                              p_demand = param_table[i, "p_demand"],
+                              f_supply = param_table[i, "f_supply"],
+                              f_demand = param_table[i, "f_demand"],
+                              inter = param_table[i, "inter"])
+                  }
 
 # 3. create networks ----
 ee_thresh <- c(10, 30)
@@ -21,14 +27,17 @@ es_thresh <- c(10, 30)
 thresholds <- expand.grid(ee_thresh = ee_thresh, 
                           es_thresh = es_thresh)
 
+
+
 networks <- apply(thresholds, 1, function(x) {
-  lapply(ls_sim, function(ls) {
-    network_create(ls = ls$ls, 
-                   params = ls$params,
-                   es_thresh = x[["es_thresh"]],
-                   ee_thresh = x[["ee_thresh"]])
-  })
-})
+  foreach(ls = ls_sim, 
+          .packages = c("sLandserv", "tidyverse", "sf"),
+          .export = c("x", "ls_sim")) %dopar% {
+            network_create(ls = ls$ls, 
+                           params = ls$params,
+                           es_thresh = x[["es_thresh"]],
+                           ee_thresh = x[["ee_thresh"]])
+          }})
 
 networks <- unlist(networks, recursive = FALSE)
 
