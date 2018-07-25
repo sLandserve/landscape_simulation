1dd6ad68a893279c8859fb4a2bb918f32fd3efb1
laurajanegraham <laurajaneegraham@gmail.com>
2018-07-25 12:10:11 +0100

    remove old results files and update the benefit creation script

--- es_benefit.R
+++ es_benefit.R
@@ -19,35 +19,40 @@ strt <- Sys.time()
 plan(multiprocess)
 
 # 2. create the landscape parameters ----
-system.time(
-  out <- crossing(nrow, ncol, p_supply,p_demand,f_supply,f_demand,inter) %>% 
-    # 3. simulate the landscapes ----
+benefit <- crossing(nrow, ncol, p_supply,p_demand,f_supply,f_demand,inter) %>% 
+  # 3. simulate the landscapes ----
   mutate(ls_sim = pmap(., ls_create),
-         # and get the bits we'll use later out
-         ls_supply = map(ls_sim, "ls_supply"),
-         ls_demand = map(ls_sim, "ls_demand"),
-         params = map(ls_sim, "params")) %>% 
-    
-    # 4. create the network threshold parameters ----
+       # and get the bits we'll use later out
+       ls_supply = map(ls_sim, "ls_supply"),
+       ls_demand = map(ls_sim, "ls_demand"),
+       params = map(ls_sim, "params")) %>% 
+  
+  # 4. create the network threshold parameters ----
   crossing(ee_thresh, es_thresh) %>% 
-    
-    # 5. create the networks ----
+  
+  # 5. create the networks ----
   mutate(ee_network = select(., ls_supply, ee_thresh, params) %>% 
-           mutate(supply_area = "patch_area") %>% 
-           pmap(create_ee_network),
-         params = map(ee_network, "params")) %>% 
-    mutate(es_network = select(., ls_supply, ls_demand, es_thresh, params) %>% 
-             mutate(supply_area = "patch_area", demand_area = "patch_area") %>% 
-             pmap(create_es_network),
-           # get the bits we'll use later
-           params = map(es_network, "params"),
-           ee_network = map(ee_network, "network"),
-           es_network = map(es_network, "network")) %>% 
-    
-    # 6. create benefit parameters ----
+         mutate(supply_area = "patch_area") %>% 
+         pmap(create_ee_network),
+       params = map(ee_network, "params")) %>% 
+  mutate(es_network = select(., ls_supply, ls_demand, es_thresh, params) %>% 
+           mutate(supply_area = "patch_area", demand_area = "patch_area") %>% 
+           pmap(create_es_network),
+         # get the bits we'll use later
+         params = map(es_network, "params"),
+         ee_network = map(ee_network, "network"),
+         es_network = map(es_network, "network")) %>% 
+  
+  # 6. create benefit parameters ----
   crossing(rival, alpha, beta, gamma) %>% 
-    mutate(benefit = select(., ee_network, es_network, rival, alpha, beta, gamma, params) %>% 
-             future_pmap(calculate_benefit))
-)
+  mutate(benefit = select(., ee_network, es_network, rival, alpha, beta, gamma, params) %>% 
+           future_pmap(calculate_benefit)) %>% 
+  select(benefit) %>% 
+  unnest
+  
+  # 7. output for analysis ----
+  save(benefit, file = tempfile(tmpdir = "results/benefit_replicates", fileext = ".Rda"))
+
+print(paste0("Replicate complete: ", Sys.time() - strt))
 
 
