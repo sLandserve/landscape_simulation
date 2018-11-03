a2482d316d97a544c7e30ae40b49937ca4f1e4fc
laurajanegraham <laurajaneegraham@gmail.com>
2018-11-03 13:32:51 +0000

    update es_benefit with future map functions to speed up processing

--- es_benefit.R
+++ es_benefit.R
@@ -21,11 +21,11 @@ plan(multiprocess)
 # 2. create the landscape parameters ----
 benefit <- crossing(nrow, ncol, p_supply,p_demand,f_supply,f_demand,inter) %>% 
   # 3. simulate the landscapes ----
-  mutate(ls_sim = pmap(., ls_create),
+  mutate(ls_sim = future_pmap(., ls_create),
        # and get the bits we'll use later out
-       ls_supply = map(ls_sim, "ls_supply"),
-       ls_demand = map(ls_sim, "ls_demand"),
-       params = map(ls_sim, "params")) %>% 
+       ls_supply = future_map(ls_sim, "ls_supply"),
+       ls_demand = future_map(ls_sim, "ls_demand"),
+       params = future_map(ls_sim, "params")) %>% 
   
   # 4. create the network threshold parameters ----
   crossing(ee_thresh, es_thresh) %>% 
@@ -33,15 +33,15 @@ benefit <- crossing(nrow, ncol, p_supply,p_demand,f_supply,f_demand,inter) %>%
   # 5. create the networks ----
   mutate(ee_network = select(., ls_supply, ee_thresh, params) %>% 
          mutate(supply_area = "patch_area") %>% 
-         pmap(create_ee_network),
-       params = map(ee_network, "params")) %>% 
+         future_pmap(create_ee_network),
+       params = future_map(ee_network, "params")) %>% 
   mutate(es_network = select(., ls_supply, ls_demand, es_thresh, params) %>% 
            mutate(supply_area = "patch_area", demand_area = "patch_area") %>% 
-           pmap(create_es_network),
+           future_pmap(create_es_network),
          # get the bits we'll use later
-         params = map(es_network, "params"),
-         ee_network = map(ee_network, "network"),
-         es_network = map(es_network, "network")) %>% 
+         params = future_map(es_network, "params"),
+         ee_network = future_map(ee_network, "network"),
+         es_network = future_map(es_network, "network")) %>% 
   
   # 6. create benefit parameters ----
   crossing(rival, alpha, beta, gamma) %>% 
